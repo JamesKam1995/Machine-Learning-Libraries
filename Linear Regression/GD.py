5010b55c2f9d27646c5829920577c6a891d821ce
Kam Say Hong <100778004+JamesKam1995@users.noreply.github.com>
2024-10-25 17:36:36 +0800

    Linear regression GD

---
+++ Linear Regression/GD.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class LinearRegression():
+    def __init__(self, learning_rate, iteration) -> None:
+        self.learning_rate = learning_rate
+        self.iteration = iteration 
+
+    def fit():
+        pass
+
+    def predict():
+        pass
+
+    def gradient_descent():
+        pass
