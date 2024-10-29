a4ad409abc38f41d8f6b5ed41c5d10e73d8f81b0
Kam Say Hong <100778004+JamesKam1995@users.noreply.github.com>
2024-10-29 18:06:04 +0800

    GD amend

--- Linear Regression/GD.py
+++ Linear Regression/GD.py
@@ -1,11 +1,18 @@
 import numpy as np
 
 class LinearRegression():
-    def __init__(self, learning_rate, iteration) -> None:
+    def __init__(self, learning_rate=0.01, iteration=1000, weight, bias) -> None:
         self.learning_rate = learning_rate
         self.iteration = iteration 
+        self.weight = weight
+        self.bias = bias
 
-    def fit():
+    def fit(self, X, y):
+        n_sample, n_feature = X.shape
+        self.weight = np.zeros(n_feature)
+        self.bias = 0 
+
+        for _ in range(iteration)
         pass
 
     def predict():
