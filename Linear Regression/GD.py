b6a35e439ba27462827c81a56070b9f0843b68af
JamesKam1995 <kamsayhong@gmail.com>
2024-11-01 15:13:39 +0800

    linear regression

--- Linear Regression/GD.py
+++ Linear Regression/GD.py
@@ -1,7 +1,9 @@
+#ref: https://github.com/AssemblyAI-Community/Machine-Learning-From-Scratch
+
 import numpy as np
 
 class LinearRegression():
-    def __init__(self, learning_rate=0.01, iteration=1000, weight, bias) -> None:
+    def __init__(self, weight, bias, learning_rate=0.01, iteration=1000) -> None:
         self.learning_rate = learning_rate
         self.iteration = iteration 
         self.weight = weight
@@ -12,11 +14,15 @@ class LinearRegression():
         self.weight = np.zeros(n_feature)
         self.bias = 0 
 
-        for _ in range(iteration)
-        pass
+        for _ in range(self.iteration):
+            y_pred = np.dot(X, self.weight) + self.bias
 
-    def predict():
-        pass
+            dw = (1 / n_sample) * np.dot(X.T, (y_pred - y))
+            db = (1 / n_sample) * np.sum(y_pred - y)
 
-    def gradient_descent():
-        pass
+            self.weight = self.weight - self.learning_rate * dw
+            self.bias = self.bias - self.learning_rate * db
+
+    def predict(self, X):
+        y_pred = np.dot(X, self.weight) + self.bias
+        return y_pred
