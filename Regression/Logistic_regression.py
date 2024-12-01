69bf4e8174d89901f60131e1a99d6e1381af1894
JamesKam1995 <kamsayhong@gmail.com>
2024-12-02 01:21:47 +0800

    regularization logistic regression

--- Regression/Logistic_regression.py
+++ Regression/Logistic_regression.py
@@ -1,15 +1,17 @@
-ref : https://github.com/casper-hansen/Logistic-Regression-From-Scratch/blob/main/src/logistic_regression/model.py
+#ref : https://github.com/casper-hansen/Logistic-Regression-From-Scratch/blob/main/src/logistic_regression/model.py
 
 import copy
 import numpy as np
 from sklearn.metrics import accuracy_score
 
 class LogisticRegression():
-    def __init__(self, lr = 0.001, n_iters = 1000):
+    def __init__(self, lr = 0.1, n_iters = 1000, lambda_ = 0.01, penalty = 'l2'):
         self.losses = []
         self.train_accuracies = []
         self.lr = lr
         self.n_iters = n_iters
+        self.lambda_ = lambda_
+        self.penalty = penalty
         self.w = None
         self.b = None
 
@@ -17,11 +19,11 @@ class LogisticRegression():
         x = self._transform_x(x)
         y = self._transform_y(y)
 
-        self.weights = np.zeros(x.shape[1])
-        self.bias = 0
+        self.w = np.zeros(x.shape[1])
+        self.b = 0
 
         for i in range(n_iters):
-            x_dot_weights = np.matmul(self.weights, x.transpose()) + self.bias
+            x_dot_weights = np.matmul(self.w, x.transpose()) + self.b
             pred = self._sigmoid(x_dot_weights)
             loss = self.compute_loss(y, pred)
             error_w, error_b = self.compute_gradients(x, y, pred)
@@ -29,7 +31,8 @@ class LogisticRegression():
 
             pred_to_class = [1 if p > 0.5 else 0 for p in pred]
             self.train_accuracies.append(accuracy_score(y, pred_to_class))
-            self.losses.append(loss)
+            self.losses.append(loss)            
+
 
     def compute_loss(self, y_true, y_pred):
         # binary cross entropy
@@ -38,20 +41,27 @@ class LogisticRegression():
         return -np.mean(y_zero_loss + y_one_loss)
 
     def compute_gradients(self, x, y_true, y_pred):
-        # derivative of binary cross entropy
-        difference =  y_pred - y_true
+        # Derivative of binary cross entropy
+        difference = y_pred - y_true
         n_samples, n_features = x.shape
-        dw = (1/n_samples) * np.dot(x.T, (difference))
-        db = (1/n_samples) * np.sum(difference)
+        db = np.mean(difference)
+        dw = np.matmul(x.transpose(), difference)
         dw = np.array([np.mean(grad) for grad in dw])
+
+        # Apply regularization if specified
+        if self.penalty == 'l2':
+            dw += (self.lambda_ / n_samples) * self.w
+        elif self.penalty == 'l1':
+            dw += (self.lambda_ / n_samples) * np.sign(self.w)
+        
         return dw, db
 
     def update_model_parameters(self, error_w, error_b):
-        self.weights = self.weights - self.lr * error_w
-        self.bias = self.bias - self.lr * error_b
+        self.w = self.w - self.lr * error_w
+        self.b = self.b - self.lr * error_b
 
     def predict(self, x):
-        x_dot_weights = np.matmul(x, self.weights.transpose()) + self.bias
+        x_dot_weights = np.matmul(x, self.w.transpose()) + self.b
         probabilities = self._sigmoid(x_dot_weights)
         return [1 if p > 0.5 else 0 for p in probabilities]
 
