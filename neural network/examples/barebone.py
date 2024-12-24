2983c55e70b8d56f466c81fd7cf9e18d9ce424a6
JamesKam1995 <kamsayhong@gmail.com>
2024-12-24 22:26:51 +0800

    remove folder

--- neural network/examples/barebone.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torch.utils.data import sampler
-
-import torchvision.datasets as dset
-import torchvision.transforms as T
-import numpy as np
