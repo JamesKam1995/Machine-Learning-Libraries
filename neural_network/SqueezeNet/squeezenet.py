d35d526a138c4893e41bd59e76f6a46961ba3515
JamesKam1995 <kamsayhong@gmail.com>
2025-03-15 14:52:11 +0800

    test fire module

--- neural_network/SqueezeNet/squeezenet.py
+++ neural_network/SqueezeNet/squeezenet.py
@@ -11,13 +11,12 @@ class FireModule(nn.Module):
     2. Squeeze Layer
     """
     def __init__(self, in_channels, squeeze_channels, expansionX1_channels, expansionX3_channels):
-        super(FireModule).__init__()
-        self.in_channels = in_channels
+        super(FireModule, self).__init__()
         self.squeeze_channel = nn.Conv2d(in_channels=in_channels, out_channels=squeeze_channels, kernel_size=1)
         self.squeeze_activation = nn.ReLU(inplace=True)
         self.expansionX1_channels = nn.Conv2d(in_channels=squeeze_channels, out_channels=expansionX1_channels, kernel_size=1)
         self.expansionX1_activation = nn.ReLU(inplace=True)
-        self.expansionX3_channels = nn.Conv2d(in_channels=squeeze_channels, out_channels=expansionX3_channels, kernel_size=1, padding=1)
+        self.expansionX3_channels = nn.Conv2d(in_channels=squeeze_channels, out_channels=expansionX3_channels, kernel_size=3, padding=1)
         self.expansionX3_activation = nn.ReLU(inplace=True)
 
     def forward(self, x):
@@ -27,18 +26,22 @@ class FireModule(nn.Module):
         Returns:
         Tensor of shape (batch size, expansionX1_channels + expansionX3_channels, height, width)
         """
-
+        # Squeeze layer
         x = self.squeeze_activation(self.squeeze_channel(x))
-        return torch.cat(
-            self.expansionX1_activation(self.expansionX1_channels(x)),
-            self.expansionX3_activation(self.expansionX3_channels(x)), 1)
+
+        # Expansion layers
+        expand1 = self.expansionX1_activation(self.expansionX1_channels(x))
+        expand3 = self.expansionX3_activation(self.expansionX3_channels(x))
+
+        # Concatenate along the channel dimension (dim=1)
+        return torch.cat((expand1, expand3), dim=1)
 
 
 class SqueezeNet(nn.Module):
     def __init__(self, in_channels, num_classess=1000):
         super(SqueezeNet, self).__init__()
         #conv
-        self.conv1 = nn.Conv2d(in_channels, out_channels=?, stride=1, padding=0) #check 
+        #self.conv1 = nn.Conv2d(in_channels, out_channels, stride=1, padding=0) #check 
         #MaxPool
         #fire module
         """
